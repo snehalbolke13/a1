c9b5b3ee55c4803b49dc9104a5ee39dae6ef7f09
abc <abcgmail.com>
2024-04-04 23:22:22 +0530

    add file

---
+++ add.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int add(int a,int b)
+{
+	cout<<a+b;
+}
+int main()
+{    int x,y,result;
+	cout<<"enter the number of x";
+	cin>>x;
+	cout<<"enter the number of y";
+	cin>>y;
+	add(x,y);
+}
