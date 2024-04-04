6e73c27a459528baead00fbe8bc53ce8e49ef425
abc <abcgmail.com>
2024-04-04 23:29:09 +0530

    multiply file

--- add.cpp
+++ add.cpp
@@ -4,6 +4,10 @@ int add(int a,int b)
 {
 	cout<<a+b;
 }
+int multiply(int a,int b)
+{
+	cout<<"\n"<<a*b;
+}
 int main()
 {    int x,y,result;
 	cout<<"enter the number of x";
@@ -11,4 +15,5 @@ int main()
 	cout<<"enter the number of y";
 	cin>>y;
 	add(x,y);
+	multiply(x,y);
 }
